5fd2766d4718c116cff6a84fd46eeb0cf5db5d03
salvador.pulido <salvador.pulido@106-23.info2>
2025-10-14 13:44:50 +0200

    ejercicio 3 sin terminar

---
+++ src/diagramas/Exercise03.java
@@ -0,0 +1,55 @@
+package diagramas;
+
+import java.util.Scanner;
+
+public class Exercise03 {
+
+	public static void main(String[] args) {
+		
+		//Abrimos scanner
+		
+		Scanner reader = new Scanner(System.in);
+		
+		//Declaramos variables
+		
+		int num1;
+		int num2;
+			
+		double suma;
+		double resta;
+		double multiplicacion;
+		double division;
+		
+		//Pedimos al usuario que introduzca un numero
+		
+		System.out.println("Introduzca numero");
+		num1 = reader.nextInt();
+		
+		System.out.println("Introduzca otro numero");
+		num2 = reader.nextInt();
+		
+		//Operamos
+		
+		suma = num1 + num2;
+		resta = num1 - num2;
+		multiplicacion = num1 * num2;
+		division = num1 / num2;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
