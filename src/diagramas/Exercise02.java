818f90948f33c692874f532de6ab7909bb7c553e
salvador.pulido <salvador.pulido@106-23.info2>
2025-10-14 13:03:15 +0200

    diagrama 2

---
+++ src/diagramas/Exercise02.java
@@ -0,0 +1,27 @@
+package diagramas;
+
+import java.util.Scanner;
+
+public class Exercise02 {
+	
+	public static void main(String[] args) {
+	
+		// Abrimos scanner
+		Scanner reader = new Scanner(System.in);
+	    int edad;
+	    
+	 	// Pedimos introducir edad
+	    System.out.println("Introduzca su edad");
+	    edad = reader.nextInt();
+	
+	    //Calcula e imprime un resultado u otro
+	    if (edad >= 18) {
+            System.out.println("Eres mayor de edad.");
+        } else {
+            System.out.println("Eres menor de edad.");
+        }
+	    
+	    // Cerramos el scanner
+        reader.close();
+    }
+}
