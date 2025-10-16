e49265137576db79cfdbf3cc4b23007b8690a94c
salvador.pulido <salvador.pulido@106-23.info2>
2025-10-16 13:08:10 +0200

    ejercicio 10

---
+++ src/diagramas/Exercise10.java
@@ -0,0 +1,50 @@
+package diagramas;
+
+import java.util.Scanner;
+
+public class Exercise10 {
+
+	public static void main(String[] args) {
+		
+		int horas;
+		int minutos;
+		int segundos;
+		
+		Scanner reader = new Scanner(System.in);
+		
+		System.out.println("introduzca las horas");
+		horas = reader.nextInt();
+		
+		System.out.println("introduzca los minutos");
+		minutos = reader.nextInt();
+		
+		System.out.println("introduzca los segundos");
+		segundos = reader.nextInt();
+				
+		if(segundos < 59) {
+			segundos++;
+			
+		} else {
+			segundos = 0;
+			if(minutos < 59) {
+				minutos++;
+			} else {
+				minutos = 0;
+				if(horas < 59) {
+					horas++;
+				} else {
+					horas = 0;
+					
+				}	
+				
+			}
+					
+		}
+		
+		System.out.println(horas + ":" minutos + ":" segundos);
+	
+	reader.close();
+	
+	}
+
+}
