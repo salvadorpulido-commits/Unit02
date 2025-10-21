63d109114367c91ba7990248eb9d80aceb490f6a
salvador.pulido <salvador.pulido@106-23.info2>
2025-10-21 11:56:17 +0200

    ejerecicio 4
    new exercises

---
+++ src/switch_exercises/Exercise04.java
@@ -0,0 +1,20 @@
+package switch_exercises;
+
+import java.util.Scanner;
+
+public class Exercise04 {
+
+	public static void main(String[] args) {
+		/*
+		 * Escribe un algoritmo para sumar dos tiradas de un dado de seis caras que tiraría el usuario. 
+		 * El algoritmo pregunta al usuario cuánto ha sacado en la primera tirada 
+		 * y el usuario le dará esa información pero en formato cadena (“UNO”, “DOS” … “SEIS”).
+		 * Después el algoritmo volverá a preguntar al usuario cuánto ha sacado en la segunda tirada 
+		 * y el usuario también dará esa información en formato cadena.
+		 * Por último, el algoritmo mostrará por pantalla la suma de las dos tiradas 
+		 * en formato numérico.
+		 */
+
+	}
+
+}
