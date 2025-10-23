a4e11ff530824b0ea6d584c136c0f5e6f824d6f9
salvadorpulido-commits <salvador.pulido@iesnervion.es>
2025-10-23 13:45:42 +0200

    correccion de errores

--- src/if_else/Exercise06.java
+++ src/if_else/Exercise06.java
@@ -6,7 +6,7 @@ public class Exercise06 {
 
 	public static void main(String[] args) {
 		
-		/**
+		/*
 		 * Escribir un programa que pida al usuario tres números enteros, 
 		 * y que muestre por pantalla si la suma de dos de esos números 
 		 * da como resultado el otro número.
@@ -15,9 +15,9 @@ public class Exercise06 {
 		
 		//Declaramos variables
 		
-		int num1 
-		int num2
-		int num3
+		int num1;
+		int num2;
+		int num3;
 		
 		//Abrimos Scanner
 		
