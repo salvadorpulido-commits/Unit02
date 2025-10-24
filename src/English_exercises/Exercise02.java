78c03ee648685e9715b552563fd6abac12ff2710
salvadorpulido-commits <salvador.pulido@iesnervion.es>
2025-10-24 13:44:38 +0200

    mirar git

---
+++ src/English_exercises/Exercise02.java
@@ -0,0 +1,18 @@
+package English_exercises;
+
+import java.util.Scanner;
+
+public class Exercise02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Scanner reader = new Scanner(System.in);
+		
+		reader.close();
+				
+
+	}
+
+}
