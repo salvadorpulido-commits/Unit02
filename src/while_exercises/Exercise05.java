a00b2460c98821f6026911646aac7f203ba7712d
salvadorpulido-commits <salvador.pulido@iesnervion.es>
2025-10-30 13:23:01 +0100

    incompleto

---
+++ src/while_exercises/Exercise05.java
@@ -0,0 +1,35 @@
+package while_exercises;
+
+import java.util.Scanner;
+
+public class Exercise05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int sumaEdades = 0;
+		int	numAlumnos = 0;		
+		int numMayoresEdad = 0;
+		double media;
+		
+		int edad;
+		
+		Scanner reader = new Scanner(System.in);
+		
+		System.out.println("Introduce edad ");
+		edad = reader.nextInt();
+		
+		while (edad >= 0) {
+			sumaEdades += edad;
+			numAlumnos++;
+			
+		if (edad >= 18);
+		
+		}
+		
+		
+		
+		
+	}
+
+}
