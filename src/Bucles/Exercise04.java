6a60a48736d162aead404aa4b146cd0fb174c784
salvadorpulido-commits <salvador.pulido@iesnervion.es>
2025-11-17 09:50:25 +0100

    comentarios de enunciado

--- src/Bucles/Exercise04.java
+++ src/Bucles/Exercise04.java
@@ -3,6 +3,14 @@ package Bucles;
 import java.util.Scanner;
 
 public class Exercise04 {
+	
+	/*De forma similar a la actividad 3, realiza un programa 
+	 * que calcule el mínimo común múltiplo de dos números dados. 
+	 * En este caso, habrá que partir del máximo de los dos e ir 
+	 * incrementando hasta encontrar el primer número que sea múltiplo 
+	 * de los dos números.
+	 */
+
 
     public static void main(String[] args) {
 
