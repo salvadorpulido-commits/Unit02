b32970bd232507fc18152cdd9c1bf4587dfd09e4
salvadorpulido-commits <salvador.pulido@iesnervion.es>
2025-11-24 08:53:08 +0100

    corregido en clase

---
+++ src/Bucles/Exercise10.java
@@ -0,0 +1,46 @@
+package Bucles;
+
+import java.util.Scanner;
+
+public class Exercise10 {
+
+    public static void main(String[] args) {
+
+        // Número introducido por el usuario
+        int numero;
+
+        // Número con cifras invertidas
+        int numeroInvertido = 0;
+
+        // Número auxiliar para realizar las operaciones
+        int numAux;
+
+        // Última cifra del número
+        int ultimaCifra;
+
+        // Scanner para leer el número al usuario
+        Scanner reader = new Scanner(System.in);
+
+        // Pedimos número al usuario
+        System.out.println("Introduzca un número: ");
+        numero = reader.nextInt();
+
+        // Guardamos en la variable auxiliar el número introducido por el usuario
+        numAux = numero;
+
+        // Bucle para invertir el número
+        while (numAux > 0) {
+            ultimaCifra = numAux % 10;
+            numeroInvertido = numeroInvertido * 10 + ultimaCifra;
+            numAux /= 10;
+        }
+
+        // Comprobación capicúa
+        System.out.println(numeroInvertido == numero ? "Es capicúa" : "No es capicúa");
+
+        // Cerramos scanner
+        reader.close();
+
+    }
+
+}
