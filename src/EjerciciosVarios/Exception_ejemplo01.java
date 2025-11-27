8319b1722a169cb99d3ab8d4a56dcf51040b397f
salvador.pulido <salvador.pulido@106-23.info2>
2025-11-27 14:39:08 +0100

    ejercicios nuevos corregir

---
+++ src/EjerciciosVarios/Exception_ejemplo01.java
@@ -0,0 +1,14 @@
+package EjerciciosVarios;
+
+import java.util.InputMismatchException;
+
+import java.util.Scanner;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
