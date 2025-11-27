3fb30c881fb5920b563a4df6c552a54dde3f8f37
salvador.pulido <salvador.pulido@106-23.info2>
2025-11-27 12:32:54 +0100

    corregido en clase

--- src/diagramas/Exercise03.java
+++ src/diagramas/Exercise03.java
@@ -4,52 +4,42 @@ import java.util.Scanner;
 
 public class Exercise03 {
 
-	public static void main(String[] args) {
+	
 		
-		//Abrimos scanner
-		
-		Scanner reader = new Scanner(System.in);
-		
-		//Declaramos variables
-		
-		int num1;
-		int num2;
+		public static void main(String[] args) {
+			int num1;
+			int num2;
 			
-		double suma;
-		double resta;
-		double multiplicacion;
-		double division;
-		
-		//Pedimos al usuario que introduzca un numero
-		
-		System.out.println("Introduzca numero");
-		num1 = reader.nextInt();
-		
-		System.out.println("Introduzca otro numero");
-		num2 = reader.nextInt();
-		
-		//Operamos
-		
-		suma = num1 + num2;
-		resta = num1 - num2;
-		multiplicacion = num1 * num2;
-		division = num1 / num2;
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	}
+			int suma;
+			int resta;
+			int multiplicacion;
+			double division;
+			
+			Scanner reader = new Scanner(System.in);
+			
+			System.out.println("Introduzca un número:");
+			num1 = reader.nextInt();
+			
+			System.out.println("Introduzca otro número:");
+			num2 = reader.nextInt();
+			
+			suma = num1 + num2;
+			resta = num1 - num2;
+			multiplicacion = num1 *num2;
+			
+			System.out.println("La suma es: " + suma);
+			System.out.println("La resta es: " + resta);
+			System.out.println("La multiplicación es: " + multiplicacion);
+			
+			if(num2 == 0) {
+				System.out.println("No se puede dividir por 0");
+			} else {
+				division = num1 / num2;
+				System.out.println("La división es: " + division);
+			}
+			
+			reader.close();
 
-}
+		}
+
+	}
